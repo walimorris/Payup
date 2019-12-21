5b69613acd3df2238ba0b5068254f23b310a793d
walimorris <48896622+walimorris@users.noreply.github.com>
2019-12-20 20:15:17 -0800

    Update notify_script.py

--- notify_script.py
+++ notify_script.py
@@ -1,8 +1,5 @@
 import notify2
-import os
 import sys
-import subprocess
-import datetime
 
 def main():
     # First we'll read bill information and store it
@@ -14,13 +11,11 @@ def main():
             bill_info.append(line)
             line = bill.readline()
         bill.close()
-
     # Second, we'll create a formatted piece of text for the notification
     heart = '\n\t\t\t\t\t' + chr(9829) + ' Made with Python & C\n'
     notification = ''
     for i in range(len(bill_info)):
         notification += bill_info[i] + '\n'
-
     #create image
     image = "file://" + os.path.abspath(os.path.curdir) + "/paid.png"
     #send notification
@@ -31,8 +26,6 @@ def main():
         print("Falied to send notification")
         sys.exit(1)
     
-    
-
-
-main()
+if __name__ == '__main__': 
+    main() 
     
